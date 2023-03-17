9c444e1fc9346b1c310a3cf45396eb8af9cefe81
rockroman <rock.rakic@gmail.com>
2023-03-17 23:00:16 +0000

    basic  footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,49 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return <footer className={styles.footer-distributed}>
+		             <div class="footer-left">
+    
+	<h3>Geeky<span>GIRL</span></h3>
+
+	<p className={styles.footer-links}>
+		<a href="#">Link</a>
+		·
+		<a href="#">Link</a>
+		·
+		<a href="#">Link</a>
+		·
+		<a href="#">Link</a>
+		·
+		<a href="#">Link</a>
+		·
+		<a href="#">Link</a>
+	</p>
+
+	<p className={styles.footer-companyName}>Tindy and her minions </p>
+
+	<div className={styles.footer-icons}>
+
+		<a href="#"><i class="fa fa-github"></i></a>
+		<a href="#"><i class="fa fa-github"></i></a>
+		<a href="#"><i class="fa fa-github"></i></a>
+		<a href="#"><i class="fa fa-github"></i></a>
+		<a href="#"><i class="fa fa-github"></i></a>
+
+	</div>
+
+</div>
+
+<div className={styles.footer-right}>
+
+	<div className={styles.foot-img} >
+		<img src="images\geeky_girl-removebg-preview_480.png" alt="" style="width:200px; border-radius:50%;"/>
+	</div>
+
+</div>
+
+
+	</footer>;
 };
 
 export default Footer;
