3f06e5e889c9ee3ba5bec84ec6d5f66d7db13e25
tindyc <57362474+tindyc@users.noreply.github.com>
2023-03-18 23:20:38 +0000

    Add heading to resources

--- src/routes/resources/Resources.jsx
+++ src/routes/resources/Resources.jsx
@@ -83,6 +83,8 @@ const Resources = () => {
 
 	return (
 
+		<div>
+			<h1>Resources</h1>
 		<div className={styles.resources}>
 			{resourceList.map((resource) => (
 				<div key={resource.id} className={styles.card}>
@@ -110,6 +112,7 @@ const Resources = () => {
 					</div>
 				</div>
 			))}
+		</div>
 		</div>
 	);
 };
