3daddf340ae158649436c1b01b99c91b08469f6b
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-18 23:33:23 +0000

    multiple changes
    Add font awesome script
    Add basic roadmap structure
    Remove navlink text decoration

--- src/routes/roadmap/Roadmap.jsx
+++ src/routes/roadmap/Roadmap.jsx
@@ -7,9 +7,11 @@ import styles from "./Roadmap.module.css";
 
 const Roadmap = () => {
 	return (
-		<div>
-			Roadmap code goes here
-			<VerticalTimeline>
+		<div className={styles.roadmap}>
+			<h1>
+				Get ahead of the curve with our <span>Path into Tech</span>
+			</h1>
+			<VerticalTimeline lineColor="pink">
 				<VerticalTimelineElement
 					className="vertical-timeline-element--work"
 					contentStyle={{
@@ -35,13 +37,56 @@ const Roadmap = () => {
 						Project Management, Team Leading
 					</p>
 				</VerticalTimelineElement>
-
 				<VerticalTimelineElement
-					iconStyle={{
-						background: "rgb(16, 204, 82)",
+					className="vertical-timeline-element--work"
+					contentStyle={{
+						background: "rgb(33, 150, 243)",
 						color: "#fff",
 					}}
-				/>
+					contentArrowStyle={{
+						borderRight: "7px solid  rgb(33, 150, 243)",
+					}}
+					date="2011 - present"
+					iconStyle={{
+						background: "rgb(33, 150, 243)",
+						color: "#fff",
+					}}>
+					<h3 className="vertical-timeline-element-title">
+						Creative Director
+					</h3>
+					<h4 className="vertical-timeline-element-subtitle">
+						Miami, FL
+					</h4>
+					<p>
+						Creative Direction, User Experience, Visual Design,
+						Project Management, Team Leading
+					</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className="vertical-timeline-element--work"
+					contentStyle={{
+						background: "rgb(33, 150, 243)",
+						color: "#fff",
+					}}
+					contentArrowStyle={{
+						borderRight: "7px solid  rgb(33, 150, 243)",
+					}}
+					date="2011 - present"
+					iconStyle={{
+						background: "rgb(33, 150, 243)",
+						color: "#fff",
+					}}>
+					<h3 className="vertical-timeline-element-title">
+						Creative Director
+					</h3>
+					<h4 className="vertical-timeline-element-subtitle">
+						Miami, FL
+					</h4>
+					<p>
+						Creative Direction, User Experience, Visual Design,
+						Project Management, Team Leading
+					</p>
+				</VerticalTimelineElement>
 			</VerticalTimeline>
 		</div>
 	);
