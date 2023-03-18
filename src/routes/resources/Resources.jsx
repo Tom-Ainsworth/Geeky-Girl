c3b80b2ba688ff20f4c3f180c75b74312c6926d8
tindyc <57362474+tindyc@users.noreply.github.com>
2023-03-18 15:04:32 +0000

    Create Resources card

--- src/routes/resources/Resources.jsx
+++ src/routes/resources/Resources.jsx
@@ -1,7 +1,31 @@
 import styles from "./Resources.module.css";
 
 const Resources = () => {
-	return <div>Resources</div>;
+	return    ( <div className={styles.resources}>
+        <h1>Resources</h1>
+        <ul className="cards">
+
+          <li className={styles.cards_item}>
+            <div className={styles.card}>
+              <div className={styles.card_image}><img src="https://www.datocms-assets.com/45470/1631110818-logo-react-js.png"/></div>
+              <div className={styles.card_content}>
+                <h2 className={styles.card_title}>Free React Courses</h2>
+                <p className={styles.card_text}>Learn how to build powerful interactive applications with one of the most popular JavaScript libraries. In fact, this site is built by React!</p>
+                <div className={styles.dropdown}>
+                    <button className={styles.dropbtn} htmlFor="btnControl">Links </button>
+                    <div className={styles.dropdown_content}>
+                      <a href="https://www.codecademy.com/learn/react-101">CodeCademy - Learn React</a>
+                      <a href="https://www.udemy.com/course/react-tutorial/">Udemy - React JS FrontEnd</a>
+                      <a href="https://legacy.reactjs.org/docs/getting-started.html">React Documentation</a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </li>
+
+
+        </ul>
+      </div> )
 };
 
 export default Resources;
