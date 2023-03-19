ca5adbe24f455c2f5302173a96fbc25129187f3d
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-19 23:11:56 +0000

    Format resources

--- src/routes/resources/Resources.jsx
+++ src/routes/resources/Resources.jsx
@@ -12,8 +12,7 @@ const Resources = () => {
 			id: 1,
 			title: "Free HTML Courses",
 			body: "Learning HTML is an important first step in your web development journey and an essential skill for front- and back-end developers.",
-			imageUrl:
-				htmlLogo,
+			imageUrl: htmlLogo,
 			links: [
 				{
 					id: 1,
@@ -32,13 +31,12 @@ const Resources = () => {
 				},
 			],
 		},
-		
+
 		{
 			id: 2,
 			title: "Free CSS Courses",
 			body: "These courses gives you CSS interactive practice to start adding styles and editing layouts so you can create unique stylized web pages.",
-			imageUrl:
-				cssLogo,
+			imageUrl: cssLogo,
 			links: [
 				{
 					id: 1,
@@ -61,8 +59,7 @@ const Resources = () => {
 			id: 3,
 			title: "Free React Courses",
 			body: "Learn how to build powerful interactive applications with one of the most popular JavaScript libraries. In fact, this site is built by React!",
-			imageUrl:
-				reactLogo,
+			imageUrl: reactLogo,
 			links: [
 				{
 					id: 1,
@@ -85,8 +82,7 @@ const Resources = () => {
 			id: 4,
 			title: "Free Django Courses",
 			body: "Build & deploy rich web applications and restful APIs using Django. Learn the fundamentals of building a full-featured web site using Django.",
-			imageUrl:
-				djangoLogo,
+			imageUrl: djangoLogo,
 			links: [
 				{
 					id: 1,
@@ -105,13 +101,12 @@ const Resources = () => {
 				},
 			],
 		},
-		
+
 		{
 			id: 5,
 			title: "Coding Communities",
 			body: "Find your favourite platform to collaborate and find solutions with female coders across the globe. Connect with Us now!",
-			imageUrl:
-				communitiesLogo,
+			imageUrl: communitiesLogo,
 			links: [
 				{
 					id: 1,
@@ -133,38 +128,39 @@ const Resources = () => {
 	];
 
 	return (
-
 		<div>
 			<h1>Resources</h1>
 
-		<div className={styles.resources}>
-			{resourceList.map((resource) => (
-				<div key={resource.id} className={styles.card}>
-					<img
-						src={resource.imageUrl}
-						className={styles.card_image}
-					/>
+			<div className={styles.resources}>
+				{resourceList.map((resource) => (
+					<div key={resource.id} className={styles.card}>
+						<img
+							src={resource.imageUrl}
+							className={styles.card_image}
+						/>
 
-					<div className={styles.card_content}>
-						<h2 className={styles.card_title}>{resource.title}</h2>
-						<p className={styles.card_text}>{resource.body}</p>
+						<div className={styles.card_content}>
+							<h2 className={styles.card_title}>
+								{resource.title}
+							</h2>
+							<p className={styles.card_text}>{resource.body}</p>
 
-						<div className={styles.dropdown}>
-							<span>Links</span>
-							<div className={styles.dropdown_content}>
-								{resource.links.map((link) => {
-									return (
-										<a key={link.id} href={link.url}>
-											{link.title}
-										</a>
-									);
-								})}
+							<div className={styles.dropdown}>
+								<span>Links</span>
+								<div className={styles.dropdown_content}>
+									{resource.links.map((link) => {
+										return (
+											<a key={link.id} href={link.url}>
+												{link.title}
+											</a>
+										);
+									})}
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			))}
-		</div>
+				))}
+			</div>
 		</div>
 	);
 };
