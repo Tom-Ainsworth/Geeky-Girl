83aa7ea818bd28965c6ad47da541c689beda9c0d
Benjamin Riordan <benjaminriordan@outlook.com>
2023-03-19 12:22:07 +0000

    chnages tom to styles

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,17 +1,17 @@
-import tom from "./Home.module.css";
+import styles from "./Home.module.css";
 import hero from "../../assets/images/hero.png"
-const Home = () => <div className={tom.Home}>
-    <div className={tom.hero_image_outer}>
-        <img className={tom.hero_img} src={hero} alt="geeky girl home page main images inside there is some text" />
+const Home = () => <div className={styles.Home}>
+    <div className={styles.hero_image_outer}>
+        <img className={styles.hero_img} src={hero} alt="geeky girl home page main images inside there is some text" />
     </div>     
-    <div className={tom.info_woman}>
+    <div className={styles.info_woman}>
         <h3>A current look at woman in the industry </h3>
-        <p className={tom.info_woman_p}>As of 2023, women hold 26.7% of technology jobs.
+        <p className={styles.info_woman_p}>As of 2023, women hold 26.7% of technology jobs.
             Taking a closer look, we see that of the 141,038 women who work in the tech industry,
             56% or 79,163 are women of color. Mid-size tech-sector companies led the way in promoting
             workplace diversity, boasting more than 53% of the industry's leading employers.</p>
         <h3>pay disparity</h3>
-        <p className={tom.info_woman_p}>Its overall national mean gender pay gap is 15.4pc,
+        <p className={styles.info_woman_p}>Its overall national mean gender pay gap is 15.4pc,
             while its median is 13.8pc. Like a lot of companies
             on this list, HPE said the gap is skewed in favour of men
             because of the low representation of women in higher-paid jobs </p>
