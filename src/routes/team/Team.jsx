9adbada160937f93ac8ffd63b365508c85dadbb5
Benjamin Riordan <126483448+Benjamin-Riordan@users.noreply.github.com>
2023-03-19 20:09:22 +0000

    Benjamin (#61)
    * updated to reflect current people
    
    * fixed styling issue on github logo

--- src/routes/team/Team.jsx
+++ src/routes/team/Team.jsx
@@ -1,4 +1,10 @@
 import styles from "./Team.module.css";
+import bossman from "../../assets/images/bossman.jpeg"
+import benji from "../../assets/images/benji.jpeg"
+import ignus from "../../assets/images/ignus.png"
+import rockman from "../../assets/images/rockman.jpeg"
+import tindy from "../../assets/images/tindy.jpeg"
+
 
 const Team = () => {
   return (
@@ -10,18 +16,70 @@ const Team = () => {
 
         <div className={styles.box}>
             <div className={styles.image}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+                <img src={bossman} alt="our fearless leader tom"/>
             </div>
-            <h1>Member Name</h1>
+            <h1>tom</h1>
             <h2>Full Stack Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-            </p>
+                  <p>Hey, I'm Tom! I'm a Junior Full Stack Developer at Commodities Engineering mainly using Java, and I love working with React in my own time.</p>
             <div className={styles.icons}>
-                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+                <a href="https://github.com/Tom-Ainsworth" target={"_blank"}><i className="fa fa-github"></i></a>
             </div>
         </div>
 
 		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src={tindy} alt="our creative director and the real boss"/>
+            </div>
+            <h1>tindy</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                      <a href="https://github.com/tindyc" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                      <img src={rockman} alt="does as it says on the tin he is a rock"/>
+            </div>
+            <h1>roman</h1>
+            <h2>Full Stack Developer</h2>
+                  <p>Junior Software Developer,Father and a Husband!!
+                      I’m currently learning Django
+                      👯 I’m looking to collaborate with other developers
+                      🥅 2022 Goals: Learn more about backend technologies
+                      My hobbies are Reading, coding and ocassionaly gaming</p>
+            <div className={styles.icons}>
+                <a href="https://github.com/rockroman" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src={ignus} alt="the man with all the plans"/>
+            </div>
+            <h1>ignus</h1>
+            <h2>student at ci</h2>
+                  <p>Hey, I'm ignus I'm a student at CI ,having fun learning</p>
+            <div className={styles.icons}>
+                <a href="https://github.com/ignasvilkas" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src={benji} alt="the support"/>
+            </div>
+            <h1>benji</h1>
+            <h2>student at ci</h2>
+                  <p>Hey, I'm benji! I'm a student at CI ,having fun learning.</p>
+            <div className={styles.icons}>
+                      <a href="https://github.com/Benjamin-Riordan" target={"_blank"}><i className="fa fa-github"></i></a>
+             </div>
+        </div>
+
+		{/* <div className={styles.box}>
             <div className={styles.image}>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
             </div>
@@ -32,59 +90,7 @@ const Team = () => {
             <div className={styles.icons}>
                 <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
             </div>
-        </div>
-
-		<div className={styles.box}>
-            <div className={styles.image}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
-            </div>
-            <h1>Member Name</h1>
-            <h2>Full Stack Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-            </p>
-            <div className={styles.icons}>
-                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
-            </div>
-        </div>
-
-		<div className={styles.box}>
-            <div className={styles.image}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
-            </div>
-            <h1>Member Name</h1>
-            <h2>Full Stack Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-            </p>
-            <div className={styles.icons}>
-                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
-            </div>
-        </div>
-
-		<div className={styles.box}>
-            <div className={styles.image}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
-            </div>
-            <h1>Member Name</h1>
-            <h2>Full Stack Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-            </p>
-            <div className={styles.icons}>
-                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
-            </div>
-        </div>
-
-		<div className={styles.box}>
-            <div className={styles.image}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
-            </div>
-            <h1>Member Name</h1>
-            <h2>Full Stack Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-            </p>
-            <div className={styles.icons}>
-                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
-            </div>
-        </div>
+        </div> */}
 
 
     </div>
