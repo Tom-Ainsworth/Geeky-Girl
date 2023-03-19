5d59f99e9f648cb0b8e9bad2df2f1506345ff8e0
rockroman <rock.rakic@gmail.com>
2023-03-19 11:52:18 +0000

    testemonial section with 2 cards added

--- src/routes/success-stories/SuccessStories.jsx
+++ src/routes/success-stories/SuccessStories.jsx
@@ -1,7 +1,34 @@
 import styles from "./SuccessStories.module.css";
 
 const SuccessStories = () => {
-	return <div>SuccessStories</div>;
+	return <div className={styles.testemonial}>
+
+<figure className={styles.snip1533}>
+  <figcaption>
+    <blockquote>
+      <p>"Geeky Girl empowered me to pursue a career in tech. I'm grateful for their resources and commitment to supporting women in the industry.</p>
+    </blockquote>
+    <h3>Joshua Ravenside</h3>
+    <h4>Delloite</h4>
+  </figcaption>
+</figure>
+<figure className={styles.snip1533}>
+  <figcaption>
+    <blockquote>
+      <p>Geeky Girl played a crucial role in helping me launch my tech career. Their dedication to empowering women in the field is truly admirable."</p>
+    </blockquote>
+    <h3>Ursula Muller</h3>
+    <h4>Facebook</h4>
+  </figcaption>
+</figure>
+
+
+
+
+
+
+
+	</div>;
 };
 
 export default SuccessStories;
