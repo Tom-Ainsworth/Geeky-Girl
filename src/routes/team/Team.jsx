a17670b5fb22ead8f2e701ffba7cdcc6fad3cf7b
tindyc <57362474+tindyc@users.noreply.github.com>
2023-03-19 02:06:14 +0000

    Create Dev Team Profile Cards

--- src/routes/team/Team.jsx
+++ src/routes/team/Team.jsx
@@ -1,7 +1,66 @@
 import styles from "./Team.module.css";
 
 const Team = () => {
-	return <div>WRITE TEAM CODE HERE</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.image}>
+          <img src="https://static.thenounproject.com/png/5034901-200.png" alt="" />
+        </div>
+        <h1>Member Name</h1>
+        <h2>Full Stack Developer</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+        </p>
+        <div className={styles.icons}>
+          <a href="wwww.github.com" target={"_blank"}>
+            <i className="fa fa-github"></i>
+          </a>
+        </div>
+      </div>
+
+	  <div className={styles.box}>
+        <div className={styles.image}>
+          <img src="https://static.thenounproject.com/png/5034901-200.png" alt="" />
+        </div>
+        <h1>Member Name</h1>
+        <h2>Full Stack Developer</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+        </p>
+        <div className={styles.icons}>
+          <a href="wwww.github.com" target={"_blank"}>
+            <i className="fa fa-github"></i>
+          </a>
+        </div>
+      </div>
+
+	  <div className={styles.box}>
+        <div className={styles.image}>
+          <img src="https://static.thenounproject.com/png/5034901-200.png" alt="" />
+        </div>
+        <h1>Member Name</h1>
+        <h2>Full Stack Developer</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+        </p>
+        <div className={styles.icons}>
+          <a href="wwww.github.com" target={"_blank"}>
+            <i className="fa fa-github"></i>
+          </a>
+        </div>
+      </div>
+
+
+	
+
+     
+	  
+    </div>
+  );
 };
 
 export default Team;
