90451cb9c6764aa6bf356432d121e25de46a7cc8
tindyc <57362474+tindyc@users.noreply.github.com>
2023-03-19 03:03:07 +0000

    Add responsiveness to profile cards in teams page

--- src/routes/team/Team.jsx
+++ src/routes/team/Team.jsx
@@ -2,64 +2,97 @@ import styles from "./Team.module.css";
 
 const Team = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.box}>
-        <div className={styles.image}>
-          <img src="https://static.thenounproject.com/png/5034901-200.png" alt="" />
-        </div>
-        <h1>Member Name</h1>
-        <h2>Full Stack Developer</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-        </p>
-        <div className={styles.icons}>
-          <a href="wwww.github.com" target={"_blank"}>
-            <i className="fa fa-github"></i>
-          </a>
-        </div>
-      </div>
 
-	  <div className={styles.box}>
-        <div className={styles.image}>
-          <img src="https://static.thenounproject.com/png/5034901-200.png" alt="" />
-        </div>
-        <h1>Member Name</h1>
-        <h2>Full Stack Developer</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-        </p>
-        <div className={styles.icons}>
-          <a href="wwww.github.com" target={"_blank"}>
-            <i className="fa fa-github"></i>
-          </a>
-        </div>
-      </div>
+	<div>
 
-	  <div className={styles.box}>
-        <div className={styles.image}>
-          <img src="https://static.thenounproject.com/png/5034901-200.png" alt="" />
+	<h1>Meet the Dev Team</h1>
+	<div className={styles.container}>
+
+        <div className={styles.box}>
+            <div className={styles.image}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+            </div>
+            <h1>Member Name</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
         </div>
-        <h1>Member Name</h1>
-        <h2>Full Stack Developer</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          porro incidunt amet vel quisquam optio commodi aperiam eligendi.
-        </p>
-        <div className={styles.icons}>
-          <a href="wwww.github.com" target={"_blank"}>
-            <i className="fa fa-github"></i>
-          </a>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+            </div>
+            <h1>Member Name</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
         </div>
-      </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+            </div>
+            <h1>Member Name</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+            </div>
+            <h1>Member Name</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+            </div>
+            <h1>Member Name</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
+
+		<div className={styles.box}>
+            <div className={styles.image}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwN34CvIbQcZeL5q75BmLgyUwlba6BzZWAXeXyWUBVBQ&s" alt=""/>
+            </div>
+            <h1>Member Name</h1>
+            <h2>Full Stack Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium porro incidunt amet vel quisquam optio commodi aperiam eligendi.
+            </p>
+            <div className={styles.icons}>
+                <a href="www.github.com" target={"_blank"}><i className="fa fa-github"></i></a>
+            </div>
+        </div>
 
 
+    </div>
+
+	</div>
 	
 
      
-	  
-    </div>
   );
 };
 
